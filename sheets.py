5a4a53fd6b7afee0eb8e5648ddc748c868b04e2f
larissavurraro <130619550+larissavurraro@users.noreply.github.com>
2025-05-08 18:24:50 -0300

    Create sheets.py

---
+++ sheets.py
@@ -0,0 +1,31 @@
+import os
+import gspread
+from google.oauth2.service_account import Credentials
+from datetime import datetime
+
+def get_cred():
+    return Credentials.from_service_account_file(os.environ['GOOGLE_CLOUD_CREDENTIALS'])
+
+def criar_planilha_usuario(telegram_id):
+    creds = get_cred()
+    client = gspread.authorize(creds)
+    nova = client.copy(
+        os.environ['GOOGLE_SHEETS_TEMPLATE_ID'],
+        title=f"Finanças_{telegram_id}",
+        copy_permissions=True
+    )
+    url = f'https://docs.google.com/spreadsheets/d/{nova.id}/edit'
+    return url
+
+def adicionar_gasto_sheet(telegram_id, gasto):
+    creds = get_cred()
+    client = gspread.authorize(creds)
+    planilha = client.open(f"Finanças_{telegram_id}")
+    ws = planilha.sheet1
+    # [{data}, {desc}, {valor}, {categoria}]
+    ws.append_row([
+        gasto['data'] if 'data' in gasto else datetime.today().strftime("%d/%m/%Y"),
+        gasto['descricao'],
+        gasto['valor'],
+        gasto['categoria']
+    ])
